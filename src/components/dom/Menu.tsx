76ae7d40ca9275a956090b96ff4b7c0b372bab31
Erik Edmonds <erikedmonds2019@u.northwestern.edu>
2025-07-04 12:05:08 -0400

    Full screen pages and centered blurred menu page

--- src/components/dom/Menu.tsx
+++ src/components/dom/Menu.tsx
@@ -1,3 +1,25 @@
+'use client'
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const Avatar = dynamic(() => import('@/components/canvas/Avatar').then(mod => mod.Avatar), { ssr: false })
+const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
+const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.View), {
+  ssr: false,
+  loading: () => (
+    <div className='flex h-96 w-full flex-col items-center justify-center'>
+      <svg className='-ml-1 mr-3 h-5 w-5 animate-spin text-black' fill='none' viewBox='0 0 24 24'>
+        <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        />
+      </svg>
+    </div>
+  ),
+})
+
 export const Menu = (props) => {
   const { onSectionChange, menuOpened, setMenuOpened } = props;
 
@@ -5,7 +27,7 @@ export const Menu = (props) => {
     <>
       <button
         onClick={() => setMenuOpened(!menuOpened)}
-        className="z-20 fixed top-12 right-12 p-3 bg-[#ff8e38] w-11 h-11 rounded-md"
+        className="z-20 fixed top-12 right-12 p-2 bg-[#ff8e38] w-11 h-11 rounded-2xl"
       >
         <div
           className={`bg-white h-0.5 rounded-md w-full transition-all ${
@@ -24,12 +46,12 @@ export const Menu = (props) => {
         />
       </button>
       <div
-        className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
-      ${menuOpened ? "w-80" : "w-0"}`}
+        className={`z-10 fixed top-0 right-0 bottom-0 backdrop-blur-xl transition-all duration-1000 overflow-hidden flex flex-col
+      ${menuOpened ? "w-screen" : "w-0"}`}
       >
-        <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
+        <div className="flex-1 flex justify-center flex-col gap-6 p-8">
+          {/* TODO: Add function for onClick */}
           <MenuButton label="About" onClick={() => onSectionChange(0)} />
-          <MenuButton label="Skills" onClick={() => onSectionChange(1)} />
           <MenuButton label="Projects" onClick={() => onSectionChange(2)} />
           <MenuButton label="Contact" onClick={() => onSectionChange(3)} />
         </div>
